324d4f8407ddea17e1001cd4b8a901e5a05240d6
jAlexgz <jagomezsac2006@elvallecolegio.edu.gt>
2024-07-20 19:17:58 -0600

    Templates modificados para mostrar post desde base de datos.

--- blog/views.py
+++ blog/views.py
@@ -4,3 +4,12 @@ from django.shortcuts import render
 
 def post_list(request):
     return render(request, 'blog/post_list.html', {})
+
+
+from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
+
+def post_list(request):
+    posts = Post.objects.all()
+    return render(request, 'blog/post_list.html', {'posts': posts})
